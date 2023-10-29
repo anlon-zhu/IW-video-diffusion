bf4e69c4445234294684910a9d75c6f3a63c2458
Anlon Zhu <zhu.anlon@gmail.com>
2023-10-29 14:20:41 -0400

    baby train

--- video_diffusion_pytorch/train.py
+++ video_diffusion_pytorch/train.py
@@ -22,8 +22,8 @@ trainer = Trainer(
     './data',                         # this folder path needs to contain all your training data, as .gif files, of correct image size and number of frames
     train_batch_size=32,
     train_lr=1e-4,
-    save_and_sample_every=1000,
-    train_num_steps=700000,         # total training steps
+    save_and_sample_every=100,
+    train_num_steps=1000,         # total training steps
     gradient_accumulate_every=2,    # gradient accumulation steps
     ema_decay=0.995,                # exponential moving average decay
     amp=True                        # turn on mixed precision
